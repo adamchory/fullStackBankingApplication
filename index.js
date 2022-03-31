7ff228f034d412cb879a50ad032114dce377d36d
Adam Chory <93945214+adamchory@users.noreply.github.com>
2022-03-31 16:46:42 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ var express = require('express');
 var app     = express();
 var cors    = require('cors');
 var dal     = require('./dal.js');
+var admin   = require('./admin.js');
 const e = require('express');
 
 // used to serve static files from public directory
 app.use(express.static('public'));
 app.use(cors());
+
+// verify token at the root route
+app.get('/auth', function(req,res){
+    // read token from header
+    const idToken = req.headers.authorization
+    console.log('header:', idToken);
+
+    if (!idToken) {
+      res.status(401).send();
+      return
+    } 
+    //check, did they pass us the token?
+    //if not, do a 401 error
+    //check if verify id token was successful
+    //if not, do 401
+
+    //verify token, is this token valid?
+    admin.auth().verifyIdToken(idToken)
+        .then(function(decodedToken) {
+            console.log('decodedToken:',decodedToken);
+            res.send('Authentication Success!');
+        }).catch(function(error) {
+            console.log('error:', error);
+            res.status(401).send("Token invalid!");
+        });
+})
+
 
 // create user account
 app.get('/account/create/:name/:email/:password', function (req, res) {
